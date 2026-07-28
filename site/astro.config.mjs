cd35e9fc14b4de3000773e03334d931031589f33
Leonhard Chen <chen.leonhard@gmail.com>
2026-07-28 05:47:07 +0200

    setup gh-pages astro

---
+++ site/astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://vt2-tourney-balance.github.io',
+  base: '/Tourney-Balance-Open-Beta',
+});
