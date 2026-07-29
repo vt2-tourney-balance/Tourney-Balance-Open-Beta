651e5bca31b04ae7cbf3184fa2c7a6dfca226c6d
Leonhard Chen <chen.leonhard@gmail.com>
2026-07-29 22:08:25 +0200

    Ping prioriy isjya

--- scripts/mods/TourneyBalance/changes/talent_changes.lua
+++ scripts/mods/TourneyBalance/changes/talent_changes.lua
@@ -2297,7 +2297,7 @@ OutlineSettings.colors.tb_judged_special = {
 }
 OutlineSettings.templates.tb_judged_special = {
 	method = "ai_alive",
-	priority = 15,
+	priority = 10,
 	outline_color = OutlineSettings.colors.tb_judged_special,
 	flag = OutlineSettings.flags.non_wall_occluded,
 }
