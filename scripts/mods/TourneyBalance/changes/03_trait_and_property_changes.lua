fc49d6b015c75c1f44603144a474ad408c3ff58f
Leonhard Chen <chen.leonhard@gmail.com>
2026-08-23 05:27:42 +0200

    Remove scrounger from WS ability

--- scripts/mods/TourneyBalance/changes/03_trait_and_property_changes.lua
+++ scripts/mods/TourneyBalance/changes/03_trait_and_property_changes.lua
@@ -1,6 +1,17 @@
 local mod = get_mod("TourneyBalance")
 
 
+-- Scrounger (ammo on crit) shouldn't proc from Trueshot Volley/ability-fired arrows
+mod:hook(ProcFunctions, "ammo_fraction_gain_on_crit_trait", function (func, owner_unit, buff, params)
+	local ranged_buff_type = params[5]
+
+	if ranged_buff_type == "RANGED_ABILITY" then
+		return
+	end
+
+	return func(owner_unit, buff, params)
+end)
+
 -- Boon of Shallya 40%
 local trait_data = WeaponTraits.traits.necklace_increased_healing_received
 if trait_data and trait_data.description_values and trait_data.description_values[1] then
